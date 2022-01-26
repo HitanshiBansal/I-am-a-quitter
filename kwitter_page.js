1143f726577f947b3b8b2247a20cca7a74798761
HitanshiBansal <87066552+HitanshiBansal@users.noreply.github.com>
2022-01-26 15:40:44 +0530

    Create kwitter_page.js

---
+++ kwitter_page.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyC-H2GX75GnqWA3OP_O_C-cOFrIamF2D-c",
+  authDomain: "kwitter-73386.firebaseapp.com",
+  databaseURL: "https://kwitter-73386-default-rtdb.firebaseio.com",
+  projectId: "kwitter-73386",
+  storageBucket: "kwitter-73386.appspot.com",
+  messagingSenderId: "271657387042",
+  appId: "1:271657387042:web:6cd6f0bf0fd0c225f0367b"
+};
+
+// Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
